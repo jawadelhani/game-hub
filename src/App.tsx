d2a60f97b1c40a03ed619223bac1c6bc5e40ce28
jawad <jawadfoot.2019@gmail.com>
2025-06-27 17:27:32 +0100

    Build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
-import { Button, ButtonGroup } from "@chakra-ui/react"
+import { Grid, grid, GridItem, Show } from "@chakra-ui/react"
 
 function App() {
   return (
-    <Button colorScheme="blue">Click me</Button>
+    //custome names for your area ,architecture of website
+    <Grid templateAreas={{
+      base: ` "nav" "main" `,  //base for mobile screens
+      lg: ` "nav nav" 
+            "aside main" `   //lg for large screens 1024px
+    }}>  
+
+      <GridItem area='nav' bg='blue'>Nav</GridItem>
+      <Show above="lg"> {/* show component aside only on large screens */}
+        <GridItem area='aside' bg='gold'>aside</GridItem>
+      </Show>      
+      <GridItem area='main' bg='red'>main</GridItem>
+      
+
+    </Grid>
   )
   
 }
--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,6 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider>
       <App />
-    </ChakraProvider>,
-  </React.StrictMode>,
+    </ChakraProvider>
+  </React.StrictMode>
 )
