9568af96cfe22317d93eb355b91b0151cc4563ad
jawad <jawadfoot.2019@gmail.com>
2025-06-28 16:34:01 +0100

    Displaying plaatforms icons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,13 +1,21 @@
+//this is called a custom hook, it is a function that starts with "use" and it contains logic that can be reused across components
+//it's also called a module, it can be imported in any component 
+
 import { useEffect, useState } from 'react';
 import apiClient from '../services/api-client';
 import { CanceledError } from 'axios';
 
-
+export interface Platform{
+    id: number;
+    name: string;
+    slug: string;
+}
 //these are the types of the data we get ,it's defined in the website /api documentation
 export interface Game{
     id: number;
     name: string;
     background_image: string;
+    parent_platforms: {platform: Platform}[];
 }
 interface fetchGamesResponse {
     count: number;
