3fb1ab289c2725d3aef05d36dc2bd5ce9acbac91
jawad <jawadfoot.2019@gmail.com>
2025-07-11 13:49:36 +0100

    Handle games with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -4,9 +4,10 @@
 
 //it adds the word 'crop/600/400/' to make the image smaller
 
+import noImage from '../assets/No-Image-Placeholder.jpg'
 
 const getCroppedImageUrl=(url:string)=>{
-    if(!url) return ''
+    if(!url) return noImage
     const index = url.indexOf('media/') +'media/'.length; //index of the word 'media/' + it's length which is 6
     return url.slice(0, index) + 'crop/600/400/' + url.slice(index);
 }
