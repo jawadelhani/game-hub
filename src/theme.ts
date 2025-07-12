9b1382e05ae6c6dcab5634fdfa79e949f5987ba6
jawad <jawadfoot.2019@gmail.com>
2025-07-12 19:14:54 +0100

    Customize the theme to get darker grays

--- src/theme.ts
+++ src/theme.ts
@@ -6,6 +6,20 @@ const config: ThemeConfig ={
     initialColorMode: "dark",  //initial colorof app 
 }
 
-const theme = extendTheme({config})  //create a theme based on the config
+const theme = extendTheme({config,colors:{
+    gray:{
+        50:'#f9f9f9',
+        100:'#ededed',
+        200:'#d3d3d3',
+        300:'#b3b3b3',
+        400:'#a0a0a0',
+        500:'#898989',
+        600:'#6c6c6C',
+        700:'#202020',
+        800:'#121212',
+        900:'#111'
+
+    }
+}})  //create a theme based on the config
 
 export default theme;
